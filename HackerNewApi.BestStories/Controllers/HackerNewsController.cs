e0fcea34b807bb4b81caa37d1354597bd140dec1
beingms <164915753+beingms@users.noreply.github.com>
2024-03-25 22:30:01 +0500

    Added

---
+++ HackerNewApi.BestStories/Controllers/HackerNewsController.cs
@@ -0,0 +1,43 @@
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+using Newtonsoft.Json;
+
+namespace HackerNewApi.BestStories.Controllers
+{
+
+    public class Story
+    {
+        public Story()
+        {
+
+        }
+        public string By { get; set; }
+        public int Descendants { get; set; }
+        public int Id { get; set; }
+        public List<int> Kids { get; set; }
+        public int Score { get; set; }
+        public int Time { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HackerNewsController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            HackerNewsApiClient client = new HackerNewsApiClient();
+            var result = await client.GetBestStoriesIdsAsync();
+            return new JsonResult(result.OrderByDescending(x=>x.Score).ToList());
+        }
+     
+      
+
+    }
+}
---
+++ HackerNewApi.BestStories/Controllers/RESTClient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using HackerNewApi.BestStories.Controllers;
+using Newtonsoft.Json;
+
+public class HackerNewsApiClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly SemaphoreSlim _semaphore;
+
+    public HackerNewsApiClient()
+    {
+        var httpClientHandler = new HttpClientHandler
+        {
+            MaxConnectionsPerServer = 10 // Limiting to 10 concurrent connections per server
+        };
+
+        _httpClient = new HttpClient(httpClientHandler);
+        _semaphore = new SemaphoreSlim(10); //  Limiting to 10 concurrent requests
+    }
+
+    public async Task<List<Story>> GetBestStoriesIdsAsync()
+    {
+        List<Story> storyIds = new List<Story>();
+
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("https://hacker-news.firebaseio.com/v0/beststories.json");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                string[] splitIds = responseBody.Trim('[', ']').Split(',');
+
+                // Create tasks for fetching stories
+                Task[] tasks = new Task[splitIds.Length];
+//for (int i = 0; i < splitIds.Length; i++)
+              //  {
+                    int counter = 0;
+                    splitIds.AsParallel().ForAll(x =>
+                    {
+                        int i = Interlocked.Increment(ref counter) - 1;
+                        string id = splitIds[i].Trim();
+                        tasks[i] = FetchStoryAsync(id, storyIds);
+
+                    });
+           
+               // }
+
+                // Await all tasks to complete
+                await Task.WhenAll(tasks);
+                return storyIds;
+            }
+            else
+            {
+                Console.WriteLine($"Failed to fetch best stories IDs. Status code: {response.StatusCode}");
+            }
+
+           
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while fetching best stories IDs: {ex.Message}");
+
+        }
+        return storyIds;
+    }
+
+    private async Task FetchStoryAsync(string storyId, List<Story> storyIds)
+    {
+        try
+        {
+            // Acquire semaphore to limit concurrent requests
+            await _semaphore.WaitAsync();
+
+            HttpResponseMessage response = await _httpClient.GetAsync($"https://hacker-news.firebaseio.com/v0/item/{storyId}.json");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Process story data as needed
+                var story = JsonConvert.DeserializeObject<Story>(responseBody);
+                if (story != null)
+                    storyIds.Add(story);
+            }
+            else
+            {
+                Console.WriteLine($"Failed to fetch story {storyId}. Status code: {response.StatusCode}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred while fetching story {storyId}: {ex.Message}");
+        }
+        finally
+        {
+            // Release semaphore after request completes
+            _semaphore.Release();
+        }
+    }
+
+}
